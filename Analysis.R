371fc669d34eec4e601de9391ff3e054f5071621
Cameron White <cameron.white@fotlinc.com>
2019-06-19 14:57:21 -0500

    Added two new certs and read_all_certs() function

--- Analysis.R
+++ Analysis.R
@@ -13,11 +13,22 @@ git.certs <- do.call('rbind',lapply(paste('Courses/Git/',list.files("Courses/Git
   
 sheets.certs <- do.call('rbind',lapply(paste('Courses/Spreadsheets/',list.files("Courses/Spreadsheets"),sep=''),read_certification))
   
-shell.certs <-do.call('rbind',lapply(paste('Courses/Shell/',list.files("Courses/Shell"),sep=''),read_certification)) 
+shell.certs <- do.call('rbind',lapply(paste('Courses/Shell/',list.files("Courses/Shell"),sep=''),read_certification)) 
 
 # Combine into single dataset
 
 certs <- bind_rows(list(R = r.certs, Python = python.certs, SQL = sql.certs, Git = git.certs, Spreadsheets = sheets.certs, Shell = shell.certs), .id = 'Language')
 
 # Simplify above process into single function call
+
+read_all_certs <- function(languages = c('R','Python','SQL','Spreadsheets','Shell','Git')){
+  file_paths <- paste('Courses/',languages, '/', sep = '')
+  cert_list <- list()
+  length(cert_list) <- length(languages)
+  names(cert_list) <- languages
+  for(i in 1:length(languages)){
+    cert_list[[i]] <- do.call('rbind',lapply(paste(file_paths[i],list.files(file_paths[i]),sep=''),read_certification)) 
+  }
+  return(dplyr::bind_rows(cert_list, .id = 'Language'))
+}
 
